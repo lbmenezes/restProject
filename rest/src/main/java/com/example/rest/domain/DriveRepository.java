bd6c0d5106a23329db24525cbc0bcd8c97af75b2
lbmenezes <lbmenezes@yahoo.com.br>
2026-09-17 16:10:49 -0300

    Criação da classe Driver

---
+++ rest/src/main/java/com/example/rest/domain/DriveRepository.java
@@ -0,0 +1,5 @@
+package com.example.rest.domain;
+
+public interface DriveRepository {
+
+}
---
+++ rest/src/main/java/com/example/rest/domain/Driver.java
@@ -0,0 +1,18 @@
+package com.example.rest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Driver {
+
+    @Id
+    Long id;
+    String name;
+    LocalDate birthDate;
+}
+
